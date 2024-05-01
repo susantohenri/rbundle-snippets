13dfd7b1dba2872c9a4e9aa47ba4616f969a4a81
Henri Susanto <henry.dinus@gmail.com>
2024-05-01 12:05:20 +0700

    add filter area on create fo31

--- filter-option-728&431-with-5266&5267.php
+++ filter-option-728&431-with-5266&5267.php
@@ -1,11 +1,9 @@
 <?php
-add_filter('frm_setup_edit_fields_vars', 'filter_option_fo31_by_fo36', 20, 3);
-function filter_option_fo31_by_fo36($values, $field, $entry_31_id)
-{
-    if (in_array($field->id, [728, 431])) {
-        global $wpdb;
-        $fo36fi_id = 728 == $field->id ? 5266 : 5267;
-        $fo36options = $wpdb->get_var("
+add_filter('frm_setup_edit_fields_vars', function ($values, $field, $entry_31_id) {
+	if (in_array($field->id, [728, 431])) {
+		global $wpdb;
+		$fo36fi_id = 728 == $field->id ? 5266 : 5267;
+		$fo36options = $wpdb->get_var("
 			SELECT fo36fi.meta_value
 			FROM (
 				SELECT item_id, meta_value
@@ -24,12 +22,31 @@ function filter_option_fo31_by_fo36($values, $field, $entry_31_id)
 			) fo31fi5069 ON fo31fi5069.meta_value = fo36fi898.meta_value
 			WHERE fo31fi5069.item_id = {$entry_31_id}
 		");
-        if ($fo36options) {
-            if (str_contains($fo36options, 'a:')) $fo36options = unserialize($fo36options);
-            $values['options'] = array_filter($values['options'], function ($option) use ($fo36options) {
-                return is_array($fo36options) ? in_array($option, $fo36options) : $option == $fo36options;
-            });
-        }
-    }
-    return $values;
-}
+		if ($fo36options) {
+			if (str_contains($fo36options, 'a:')) $fo36options = unserialize($fo36options);
+			$values['options'] = array_filter($values['options'], function ($option) use ($fo36options) {
+				return is_array($fo36options) ? in_array($option, $fo36options) : $option == $fo36options;
+			});
+		}
+	}
+	return $values;
+}, 20, 3);
+
+add_filter('frm_setup_new_fields_vars', function ($values, $field) {
+	if (in_array($field->id, [728, 431]) && isset($_GET['entry_36'])) {
+		global $wpdb;
+		$fo36fi_id = 728 == $field->id ? 5266 : 5267;
+		$fo36options = $wpdb->get_var("
+			SELECT meta_value
+			FROM {$wpdb->prefix}frm_item_metas
+			WHERE field_id = {$fo36fi_id} AND item_id = {$_GET['entry_36']}
+		");
+		if ($fo36options) {
+			if (str_contains($fo36options, 'a:')) $fo36options = unserialize($fo36options);
+			$values['options'] = array_filter($values['options'], function ($option) use ($fo36options) {
+				return is_array($fo36options) ? in_array($option, $fo36options) : $option == $fo36options;
+			});
+		}
+	}
+	return $values;
+}, 20, 2);
