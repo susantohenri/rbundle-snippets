a87b03c3b8761bcb8dbbf93c62a40c5f75d66d50
Henri Susanto <henry.dinus@gmail.com>
2024-05-12 13:35:14 +0700

    list down service name done

--- form58-footer.js
+++ form58-footer.js
@@ -1,5 +1,4 @@
 (function (script) {
-    const url = window.location.href
     const form_58 = jQuery(script).parent()
     const field_884 = form_58.find(`[name="item_meta[884][]"]`)
     const field_885 = form_58.find(`[name="item_meta[885]"]`)
@@ -9,7 +8,7 @@
     const field_3713 = form_58.find(`[name="item_meta[3713]"]`)
     const section_4556 = form_58.find(`#frm_field_4556_container > div`)
     const field_5368 = form_58.find(`[name="item_meta[5368]"]`)
-    const bundled_service = form_58.find(`#frm_field_5412_container`)
+    const bundled_services = form_58.find(`#frm_field_5412_container`)
     const single_service = form_58.find(`#frm_field_881_container`)
 
     setTimeout(bundled_service_init, 1000)
@@ -115,13 +114,48 @@
         let single_service_clone = single_service.clone()
         single_service_clone.find(`select,[type="text"]`).each(function () {
             const select = jQuery(this)
-            select.attr(`name`, select.attr(`name`).replace(`item_meta`, `bundled_under`) + `[]`)
+            select.attr(`name`, select.attr(`name`).replace(`item_meta`, `bundled_children`) + `[]`)
         })
         single_service_clone = single_service_clone.html()
 
-        const bundled_service_htmls = {
-            bundled_parents: `<div class="bundled_parents"><h3 class="text-center frm_pos_top frm_section_spacing">Repeater</h3></div>`,
-            bundled_parent: `
+        let buttons = {
+            add: `
+                <br>
+                <div class="frm_form_field frm_hidden_container frm_repeat_buttons">
+                    <a href="javascript:;" class="frm_add_form_row frm_button" data-parent="" aria-label="Add">
+                        <svg viewBox="0 0 20 20" width="1em" height="1em" class="frmsvg frm-svg-icon">
+                            <title>plus1</title>
+                            <path d="M11 5H9v4H5v2h4v4h2v-4h4V9h-4V5zm-1-5a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z"></path>
+                        </svg> Add #PART#
+                    </a>
+                </div>
+            `,
+            remove: `
+                <div class="frm_form_field frm_hidden_container frm_repeat_buttons">
+                    <a href="javascript:;" class="remove_form_row frm_button" data-key="0" aria-label="Remove">
+                        <svg viewBox="0 0 20 20" width="1em" height="1em" class="frmsvg frm-svg-icon">
+                            <title>minus1</title>
+                            <path d="M5 9v2h10V9H5zm5-9a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z"></path>
+                        </svg> Remove #PART#
+                    </a>
+                </div>
+            `
+        }
+        buttons.add_bundle = buttons.add.replace(`#PART#`, `Bundle`)
+        buttons.remove_bundle = buttons.remove.replace(`#PART#`, `Bundle`)
+        buttons.add_service = buttons.add.replace(`#PART#`, `Service`)
+        buttons.remove_service = buttons.remove.replace(`#PART#`, `Service`)
+
+        let templates = {
+            children: `
+                <div class="bundled_children">
+                    ${single_service_clone}
+                    ${buttons.remove_service}
+                </div>`
+        }
+        templates.bundle = `
+            <br>
+            <div class="bundled_service">
                 <div class="bundled_parent">
                     <div class="frm_form_field form-field  form-group frm_top_container">
                         <label class="frm_primary_label col-form-label form-label">
@@ -130,152 +164,155 @@
                         </label>
                         <div class="frm_input_group input-group frm_with_box frm_with_pre">
                             <span class="frm_inline_box input-group-text input-group-addon">Business 1</span>
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="business_name[]">
                         </div>
                     </div>
                 </div>
-            `,
-            bundled_unders: `<div class="bundled_unders"><h3 class="text-center frm_pos_top frm_section_spacing">Nested Repeater</h3></div>`,
-            bundled_under: `<div class="bundled_under">${single_service_clone}</div>`,
-            frm_add_form_row: `
-                <br>
-                <div class="frm_form_field frm_hidden_container frm_repeat_buttons ">
-                    <a href="javascript:;" class="frm_add_form_row frm_button" data-parent="" aria-label="Add">
-                        <svg viewBox="0 0 20 20" width="1em" height="1em" class="frmsvg frm-svg-icon">
-                        <title>plus1</title>
-                        <path d="M11 5H9v4H5v2h4v4h2v-4h4V9h-4V5zm-1-5a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z"></path>
-                    </svg> Add</a>
+                <div class="bundled_childrens">
+                    ${buttons.add_service}
                 </div>
-            `,
-            remove_form_row: `
-                <div class="frm_form_field frm_hidden_container frm_repeat_buttons ">
-                    <a href="javascript:;" class="remove_form_row frm_button" data-key="0" aria-label="Remove">
-                        <svg viewBox="0 0 20 20" width="1em" height="1em" class="frmsvg frm-svg-icon">
-                            <title>minus1</title>
-                            <path d="M5 9v2h10V9H5zm5-9a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z"></path>
-                        </svg> Remove
-                    </a>
-                </div>
-            `
-        }
+                ${buttons.remove_bundle}
+            </div>
+        `
+        bundled_services
+            .addClass(`bundled_services`)
+            .append(buttons.add_bundle)
 
-        bundled_service
-            .append(bundled_service_htmls.bundled_parents)
-            .find(`.bundled_parents`)
-            .append(bundled_service_htmls.frm_add_form_row)
-            .find(`.frm_add_form_row`)
-            .click(function () {
-                const template = jQuery(bundled_service_htmls.bundled_parent)
-                template
-                    .append(bundled_service_htmls.remove_form_row)
-                    .insertBefore(jQuery(this).parent())
-                    .find(`.remove_form_row`).click(function () {
+        const btn_add_bundle_container = bundled_services
+            .find(`> .frm_repeat_buttons`)
+        btn_add_bundle_container
+            .find(`> .frm_add_form_row`)
+            .click(e => { // add bundle
+                const bundled_service = jQuery(templates.bundle)
+                    .insertBefore(btn_add_bundle_container)
+
+                bundled_service_bundle_business_number()
+                bundled_service
+                    .find(`>.frm_repeat_buttons .remove_form_row`)
+                    .click(function () { // remove bundle
                         jQuery(this).parent().parent().remove()
-                        bundled_service_parent_business_number()
+                        bundled_service_children_list_down_selected_service_name()
+                        bundled_service_bundle_business_number()
                     })
-                bundled_service_parent_business_number()
-            })
-            .click()
 
-        bundled_service
-            .append(bundled_service_htmls.bundled_unders)
-            .find(`.bundled_unders`)
-            .append(bundled_service_htmls.frm_add_form_row)
-            .find(`.frm_add_form_row`)
-            .click(function () {
-                const template = jQuery(bundled_service_htmls.bundled_under)
-                const created_bu = template
-                    .append(bundled_service_htmls.remove_form_row)
-                    .insertBefore(jQuery(this).parent())
-                const bu_5368_selector = `[name="bundled_under[5368][]"]`
-                const bu_5368 = created_bu.find(bu_5368_selector)
-                const bu_884 = created_bu.find(`[name="bundled_under[884][][]"]`)
-                const bu_885 = created_bu.find(`[name="bundled_under[885][]"]`)
-                const bu_938 = created_bu.find(`[name="bundled_under[938][]"]`)
-                const bu_5057 = created_bu.find(`[name="bundled_under[5057][]"]`)
+                const btn_add_service_container = bundled_service
+                    .find(`.bundled_childrens > .frm_repeat_buttons`)
+                btn_add_service_container
+                    .find(`> .frm_add_form_row`)
+                    .click(e => { // add service
+                        const bundled_children = jQuery(templates.children)
+                            .insertBefore(btn_add_service_container)
 
-                created_bu.find(`.remove_form_row`).click(function () {
-                    jQuery(this).parent().parent().remove()
-                    bundled_service_under_list_down_selected_service_name()
-                })
+                        const bu_5368_selector = `[name="bundled_children[5368][]"]`
+                        const bu_5368 = bundled_children.find(bu_5368_selector)
+                        const bu_884 = bundled_children.find(`[name="bundled_children[884][][]"]`)
+                        const bu_885 = bundled_children.find(`[name="bundled_children[885][]"]`)
+                        const bu_938 = bundled_children.find(`[name="bundled_children[938][]"]`)
+                        const bu_5057 = bundled_children.find(`[name="bundled_children[5057][]"]`)
 
-                bu_5368.click(function () {
-                    let available_services = []
-                    template.find(bu_5368_selector).find(`option`).each(function () {
-                        available_services.push(jQuery(this).attr(`value`))
-                    })
-                    jQuery(bu_5368_selector).not(jQuery(this)).each(function () {
-                        available_services = available_services.filter(opt => {
-                            return jQuery(this).val() != opt
+                        bundled_children.find(`.remove_form_row`)
+                            .click(function () { // remove service
+                                jQuery(this).parent().parent().remove()
+                                bundled_service_children_list_down_selected_service_name()
+                            })
+
+                        bu_5368.click(function () {
+                            let available_services = []
+                            jQuery(templates.children)
+                                .find(bu_5368_selector)
+                                .find(`option`)
+                                .each(function () {
+                                    available_services.push(jQuery(this).attr(`value`))
+                                })
+
+                            jQuery(bu_5368_selector)
+                                .not(jQuery(this))
+                                .each(function () {
+                                    available_services = available_services.filter(opt => {
+                                        return jQuery(this).val() != opt
+                                    })
+                                })
+                            jQuery(this)
+                                .html(available_services.map(service => {
+                                    return `<option value="${service}">${service}</option>`
+                                }))
                         })
-                    })
-                    jQuery(this).html(available_services.map(service => {
-                        return `<option value="${service}">${service}</option>`
-                    }))
-                })
 
-                bu_cond_logic_884()
-                field_3712.change(bu_cond_logic_884)
-                bu_5368.change(bu_cond_logic_884)
-                function bu_cond_logic_884() {
-                    const val_bu_5368 = bu_5368.val()
-                    if (`The selected Customer/Client` == field_3712.val() || `` == val_bu_5368) toggle(bu_884, `hide`)
-                    else if (-1 < val_bu_5368.indexOf(`Legal`) || -1 < val_bu_5368.indexOf(`IRS`)) toggle(bu_884, `hide`)
-                    else toggle(bu_884, `show`)
-                }
+                        bu_cond_logic_884()
+                        field_3712.change(bu_cond_logic_884)
+                        bu_5368.change(bu_cond_logic_884)
+                        function bu_cond_logic_884() {
+                            const val_bu_5368 = bu_5368.val()
+                            if (`The selected Customer/Client` == field_3712.val() || `` == val_bu_5368) toggle(bu_884, `hide`)
+                            else if (-1 < val_bu_5368.indexOf(`Legal`) || -1 < val_bu_5368.indexOf(`IRS`)) toggle(bu_884, `hide`)
+                            else toggle(bu_884, `show`)
+                        }
 
-                bu_cond_logic_885()
-                field_3712.change(bu_cond_logic_885)
-                bu_5368.change(bu_cond_logic_885)
-                function bu_cond_logic_885() {
-                    const val_bu_5368 = bu_5368.val()
-                    if (`The selected Customer/Client` == field_3712.val() || `` == val_bu_5368) toggle(bu_885, `show`)
-                    else if (-1 < val_bu_5368.indexOf(`Legal`) || -1 < val_bu_5368.indexOf(`IRS`)) toggle(bu_885, `show`)
-                    else toggle(bu_885, `hide`)
-                }
+                        bu_cond_logic_885()
+                        field_3712.change(bu_cond_logic_885)
+                        bu_5368.change(bu_cond_logic_885)
+                        function bu_cond_logic_885() {
+                            const val_bu_5368 = bu_5368.val()
+                            if (`The selected Customer/Client` == field_3712.val() || `` == val_bu_5368) toggle(bu_885, `show`)
+                            else if (-1 < val_bu_5368.indexOf(`Legal`) || -1 < val_bu_5368.indexOf(`IRS`)) toggle(bu_885, `show`)
+                            else toggle(bu_885, `hide`)
+                        }
 
-                bu_5368.change(e => {
-                    jQuery.post(frm_js.ajax_url, {
-                        action: `frm_get_lookup_text_value`,
-                        parent_fields: [5368],
-                        parent_vals: [bu_5368.val()],
-                        field_id: 938,
-                        nonce: frm_js.nonce
-                    }, service_name => {
-                        bu_938.val(service_name)
-                        bundled_service_under_list_down_selected_service_name()
+                        bu_5368.change(e => {
+                            jQuery.post(frm_js.ajax_url, {
+                                action: `frm_get_lookup_text_value`,
+                                parent_fields: [5368],
+                                parent_vals: [bu_5368.val()],
+                                field_id: 938,
+                                nonce: frm_js.nonce
+                            }, service_name => {
+                                bu_938.val((new DOMParser().parseFromString(service_name, `text/html`)).documentElement.textContent)
+                                bundled_service_children_list_down_selected_service_name()
+                            })
+                            jQuery.post(frm_js.ajax_url, {
+                                action: `frm_get_lookup_text_value`,
+                                parent_fields: [5368],
+                                parent_vals: [bu_5368.val()],
+                                field_id: 5057,
+                                nonce: frm_js.nonce
+                            }, service_cat => {
+                                bu_5057.val((new DOMParser().parseFromString(service_cat, `text/html`)).documentElement.textContent)
+                            })
+                        })
+
                     })
-                    jQuery.post(frm_js.ajax_url, {
-                        action: `frm_get_lookup_text_value`,
-                        parent_fields: [5368],
-                        parent_vals: [bu_5368.val()],
-                        field_id: 5057,
-                        nonce: frm_js.nonce
-                    }, service_cat => {
-                        bu_5057.val(service_cat)
-                    })
-                })
+                    .click()
             })
             .click()
     }
 
-    function bundled_service_parent_business_number() {
-        jQuery(`.bundled_parent`).each(function () {
+    function bundled_service_bundle_business_number() {
+        let bus_num = 1
+        jQuery(`.bundled_service`).each(function () {
             jQuery(this)
                 .find(`.frm_inline_box.input-group-text.input-group-addon`)
-                .html(`Business ${jQuery(this).index() - 1}`)
+                .html(`Business ${bus_num}`)
+            bus_num++
         })
     }
 
-    function bundled_service_under_list_down_selected_service_name() {
-        let selected_service_names = []
-        bundled_service
-            .find(`.bundled_unders`)
-            .find(`[name="bundled_under[938][]"]`)
+    function bundled_service_children_list_down_selected_service_name() {
+        let bundled_services_data = []
+        bundled_services
+            .find(`.bundled_service`)
             .each(function () {
-                const service_name = jQuery(this).val()
-                if (`` != service_name) selected_service_names.push(service_name)
+                const bundled_service = jQuery(this)
+                let list = { business_name: ``, service_names: [] }
+                list.business_name = bundled_service
+                    .find(`[name="business_name[]"]`)
+                    .val()
+                bundled_service
+                    .find(`[name="bundled_children[938][]"]`)
+                    .each(function () {
+                        list.service_names.push(jQuery(this).val())
+                    })
+                bundled_services_data.push(list)
             })
-        jQuery(`[name="item_meta[5365]"]`).val(selected_service_names.join(`,`))
+        jQuery(`[name="item_meta[5365]"]`).html(JSON.stringify(bundled_services_data))
     }
 })(document.currentScript);
